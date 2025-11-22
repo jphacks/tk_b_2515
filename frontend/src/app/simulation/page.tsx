8836df0458a701d9914dd36be9538bd753dd1fe0
Daccho <dacchotech@gmail.com>
2025-11-22 10:43:36 +0900

    debugと声修正

--- frontend/src/app/simulation/page.tsx
+++ frontend/src/app/simulation/page.tsx
@@ -496,7 +496,7 @@ export default function SimulationPage() {
                     />
                   </div>
                   <div className="absolute bottom-2 left-2 bg-black/50 text-white text-xs px-2 py-1 rounded">
-                    女性アバター
+                    養蜂とコラボ!!
                   </div>
                 </button>
 
